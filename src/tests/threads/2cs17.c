f36ebd0d178fb32938daa12e9477b72d4499df2b
metastableB <donkdennis@gmail.com>
2015-08-31 22:42:05 +0530

    [Assmt 2] Ready for submission

---
+++ src/tests/threads/2cs17.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_assign2_threads(){
+	printf("\n************************************************");
+	printf("\nHello Don Dennis%s",
+		      "\nThis is 2cs17 in tests/threads/");
+	printf("\n************************************************\n");
+}
